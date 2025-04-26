0004b8f680900a4648f8054d00e66c895c1df2e6
JISHNU <166325684+jishnuGG@users.noreply.github.com>
2025-04-26 23:28:36 +0530

    Time: 0 ms (100%), Space: 41.6 MB (69.11%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,17 +1,42 @@
-class Solution {
-    public boolean isabsent(String s,int n){
-        int count=0;
-        for(int i=0;i<n;i++){
-           if(s.charAt(i)=='A') count++;
-        }
-        if(count>=2){
-            return false;
-        }
-        return true;
+ class Solution {
+//     public boolean isabsent(String s,int n){
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//            if(s.charAt(i)=='A') count++;
+//         }
+//         if(count>=2){
+//             return false;
+//         }
+//         return true;
 
-    }
-    public boolean islate(String s,int n){
-            int count=0;
+//     }
+//     public boolean islate(String s,int n){
+//             int count=0;
+//             for(int i=0;i<n;i++){
+//                 if(s.charAt(i)=='L')
+//                 {
+//                     count++;
+//                     for(int j=i+1;j<n && s.charAt(j)=='L';j++)
+//                     {
+//                           if(s.charAt(i)=='L') count++;
+//                     }
+//                     if(count>=3) return false;
+//                 }
+//                 count=0;
+//             }
+//             return true;
+//     }
+//     public boolean checkRecord(String s) {
+//         int n=s.length();
+//         boolean absent=isabsent(s,n);
+//         boolean present=islate(s,n);
+//         return absent&&present;
+//     }
+// }
+ public boolean checkRecord(String s) {
+        int n=s.length();
+         int count=0;
+         int countabsent=0;
             for(int i=0;i<n;i++){
                 if(s.charAt(i)=='L')
                 {
@@ -22,14 +47,10 @@ class Solution {
                     }
                     if(count>=3) return false;
                 }
+                if(s.charAt(i)=='A') countabsent++;
+                if(countabsent>=2) return false;
                 count=0;
             }
             return true;
     }
-    public boolean checkRecord(String s) {
-        int n=s.length();
-        boolean absent=isabsent(s,n);
-        boolean present=islate(s,n);
-        return absent&&present;
-    }
-}
+ }
